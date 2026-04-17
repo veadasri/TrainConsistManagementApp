63b50fc5fadbf51df2dd9193da4e1bee544d5982
veadasri <vs2229@srmist.edu.in>
2026-04-18 01:06:23 +0530

    feature/UC15-SafeCargoAssignmentUsingtrycatchfinally

---
+++ App/src/main/UseCase15TrainConsisntMgmt.java
@@ -0,0 +1,47 @@
+package main;
+public class UseCase15TrainConsisntMgmt {
+
+    static class CargoSafetyException extends RuntimeException {
+        public CargoSafetyException(String message) {
+            super(message);
+        }
+    }
+
+    static class GoodsBogie {
+        String shape;
+        String cargo;
+
+        GoodsBogie(String shape) {
+            this.shape = shape;
+        }
+
+        void assignCargo(String cargo) {
+            try {
+                if (shape.equals("Rectangular") && cargo.equals("Petroleum")) {
+                    throw new CargoSafetyException("Unsafe cargo assignment");
+                }
+                this.cargo = cargo;
+                System.out.println("Cargo assigned: " + cargo);
+            } catch (CargoSafetyException e) {
+                System.out.println("Error: " + e.getMessage());
+            } finally {
+                System.out.println("Assignment attempted\n");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("=====================================");
+        System.out.println("   UC15 - Safe Cargo Assignment");
+        System.out.println("=====================================\n");
+
+        GoodsBogie b1 = new GoodsBogie("Cylindrical");
+        GoodsBogie b2 = new GoodsBogie("Rectangular");
+
+        b1.assignCargo("Petroleum");
+        b2.assignCargo("Petroleum");
+
+        System.out.println("Program continues...");
+    }
+}
